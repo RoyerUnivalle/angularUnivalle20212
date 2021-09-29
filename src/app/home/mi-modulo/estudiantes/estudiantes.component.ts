d869df13dc30c49d9a64a9c61308f44c5a640944
faster27 <dcorrealenis@gmail.com>
2021-09-29 16:06:54 -0500

    ejemplo binding e interpolación

--- src/app/home/mi-modulo/estudiantes/estudiantes.component.ts
+++ src/app/home/mi-modulo/estudiantes/estudiantes.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstudiantesComponent implements OnInit {
 
+
+ 
+
+
   constructor() { }
 
   ngOnInit() {
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,3 +1,4 @@
+import { flatten } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  colocarTexto(value:string){
+  }
+
+
+  habilitarCuadroTextoProperty=true;
+
+  usuarioRegistrado=false;
+
+  textoDeRegistro="no hay nadie registrado";
+
+  Empresa="Google";
+
+  getRegistroUsuario(){
+
+    this.usuarioRegistrado=false;
+
+
+  }
+
+
+  SetUsuarioRegistrado(event:Event){
+
+    //alert("el usuario se acaba de registar")
+    //this.textoDeRegistro="El usuario se acaba de registrar";
+
+    if((<HTMLInputElement>event.target).value=="si"){
+
+      this.textoDeRegistro="El usuario se acaba de registrar";
+
+    }else{
+
+      this.textoDeRegistro="No hay nadie registrado";
+
+
+    }
+
+
+  }
+
+  cambiaEmpresa(event:Event){
+
+    this.Empresa=(<HTMLInputElement>event.target).value;
+
+
+  }
 
   constructor() { }
 
